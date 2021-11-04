643e45a43fcb566784fedfbca6a4412a8c5d6a35
tatsuboh <erythrina.jp@gmail.com>
2021-11-05 00:11:21 +0900

    Initialize repository

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <title>laravel</title>
+</head>
+
+<body>
+    <!----- header----->
+    <!-- <header>laravel</header> -->
+    <nav>ナビ</nav>
+    <!----- /header ----->
+
+    <!----- main ----->
+    <article>
+        <!-- <h1>タイトル</h1> -->
+        <section>
+            <!-- <h2>見出し２</h2> -->
+            <p>コンテンツの内容</p>
+        </section>
+    </article>
+    <!----- /main ----->
+
+    <!----- footer ----->
+    <footer>フッター</footer>
+    <!----- /footer ----->
+</body>
+
+</html>
