96cd5f67fdb8fb40a7f965913481fe844a101d49
tatsuboh <erythrina.jp@gmail.com>
2021-11-15 23:00:44 +0900

    testcommmit

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App; //
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+//test
 
 class User extends Authenticatable
 {
